4aecfc745dcf4a5029f1329f691506b51a21ac3f
Yuyang Bai <1229956097@qq.com>
2020-07-10 15:28:53 -0400

    Reddit benchmark

--- demos/reddit/scripts/login.js
+++ demos/reddit/scripts/login.js
@@ -1,1 +1,7 @@
 
+    function onClick() {
+        //alert("1");
+        var name = document.getElementById("loginUsername")
+        window.location.href = "userpage/" + name.value + ".html";
+        //window.location.href = 'index.html';
+    }

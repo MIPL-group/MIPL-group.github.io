b2e228934cbd9392ec7b6436de7d2f1586711890
Yuyang Bai <1229956097@qq.com>
2020-07-10 15:37:57 -0400

    Reddit benchmark

--- demos/reddit/scripts/login.js
+++ demos/reddit/scripts/login.js
@@ -1,7 +1,5 @@
 
     function onClick() {
-        //alert("1");
         var name = document.getElementById("loginUsername")
         window.location.href = "userpage/" + name.value + ".html";
-        //window.location.href = 'index.html';
     }

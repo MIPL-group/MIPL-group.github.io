058bb98ef6a41317807d62a0dfbb8e19dae04aa2
Yuyang Bai <1229956097@qq.com>
2020-07-14 14:29:36 -0400

    Create submit.js

---
+++ demos/mprint/scripts/submit.js
@@ -0,0 +1,7 @@
+function onClick() {
+    document.getElementById("Submit").style.display = "block";
+}
+function onPrint() {
+    document.getElementById("Submit").style.display = "none";
+    window.location.reload();
+}
